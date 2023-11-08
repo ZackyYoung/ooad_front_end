c274e47f1ec58e4b6163204e5d191e02b8eb7456
OctCarp <octcarp@hotmail.com>
2023-11-08 17:22:42 +0800

    refactor: try to use yarn, scss and TypeScript
    and import necessary fonts and images

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
 
+import '@/styles/font.scss'
+
 const app = createApp(App);
 app.use(createVuestic());
 app.mount("#app");
