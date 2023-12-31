0f3a75155169d12f6af1166a344bfd510a5958bf
OctCarp <octcarp@hotmail.com>
2023-12-31 20:19:44 +0800

    fix(avatar): fix fetch picture repeatedly

--- src/store/picture.js
+++ src/store/picture.js
@@ -2,10 +2,11 @@ import {defineStore} from "pinia";
 import {reactive, ref} from "vue";
 import dataService from "@/service/dataService.js";
 
-export const usePictureStore = defineStore('picture', ()=>{
+export const usePictureStore = defineStore('picture', () => {
     const userAvatar = ref(null)
     const tempAvatar = reactive([])
-    async function updateAvatar(formData){
+
+    async function updateAvatar(formData) {
         return new Promise((resolve) => {
             dataService.updateAvatar(formData, resp => {
                 resolve()
@@ -15,39 +16,45 @@ export const usePictureStore = defineStore('picture', ()=>{
 
     async function fetchAvatar(campusId) {
         return new Promise((resolve) => {
-            dataService.fetchAvatar(campusId, resp =>{
-                if(resp.data.code === 0)
-                {
-                    userAvatar.value = "data:image/png;base64," + resp.data.data
-                }
+            if (userAvatar.value == null) {
+                dataService.fetchAvatar(campusId, resp => {
+                    if (resp.data.code === 0) {
+                        userAvatar.value = "data:image/png;base64," + resp.data.data
+                    }
+                    resolve()
+                })
+            } else {
                 resolve()
-            })
+            }
         })
     }
 
     async function fetchTempAvatar(campusId) {
         return new Promise((resolve) => {
-            dataService.fetchAvatar(campusId, resp =>{
-                if(resp.data.code === 0)
-                {
-                    if(!(tempAvatar.length !== 0 && tempAvatar.some((avatar) => avatar.campusId === campusId)))
-                    {
-                        tempAvatar.push({
-                            campusId: campusId,
-                            url: "data:image/png;base64," + resp.data.data
-                        })
+            if (!(tempAvatar.length !== 0 && tempAvatar.some((avatar) => avatar.campusId === campusId))) {
+                dataService.fetchAvatar(campusId, resp => {
+                    if (resp.data.code === 0) {
+                        {
+                            tempAvatar.push({
+                                campusId: campusId,
+                                url: "data:image/png;base64," + resp.data.data
+                            })
+                        }
                     }
-                }
+                    resolve()
+                })
+            } else {
                 resolve()
-            })
+            }
         })
     }
-    function pictureStoreClose()
-    {
+
+    function pictureStoreClose() {
         userAvatar.value = null
         tempAvatar.length = 0
     }
-    return{
+
+    return {
         userAvatar,
         tempAvatar,
         updateAvatar,
